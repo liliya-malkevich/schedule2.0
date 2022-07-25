b0676421479eeada965bf90cb9c719b06aa41a30
Malkevich Liliya E <MalkevichLE@bsu.by>
2022-07-25 12:52:03 +0300

    added Course select in Update

--- schedule/schedule/Controllers/ScheduleUpdateController.cs
+++ schedule/schedule/Controllers/ScheduleUpdateController.cs
@@ -9,19 +9,27 @@ namespace schedule.Controllers
         ScheduleDataAccessLayer objschedule = new ScheduleDataAccessLayer();
         GroupDataAccessLayer objgroup = new GroupDataAccessLayer();
         FormatDataAccessLayer objformat = new FormatDataAccessLayer();
-      
+      CourseDataAccessLayer objcourse = new CourseDataAccessLayer();
         LectureHallDataAccessLayer objlecturehall = new LectureHallDataAccessLayer();
         NoteDataAccessLayer objnote = new NoteDataAccessLayer();
         SubjectDataAccessLayer objsubject = new SubjectDataAccessLayer();
         TeacherDataAccessLayer objteacher = new TeacherDataAccessLayer();
         TimeLessonsDataAccessLayer objtimelesson = new TimeLessonsDataAccessLayer();
         WeekdayDataAccessLayer objweekday = new WeekdayDataAccessLayer();   
-        public IActionResult Update(int IdSchedule)
+        public IActionResult Update(int IdSchedule, int IdCourse)
         {
             //dynamic mymodel = new ExpandoObject();
             ViewModelUpdate mymodel = new ViewModelUpdate();
+            mymodel.Course = objcourse.CourseList().ToList();
+            if (IdCourse != 0)
+            {
+                mymodel.Group = objgroup.GroupCourseList(IdCourse).ToList();
+            }
+            if(IdCourse == 0)
+            {
+                mymodel.Group = objgroup.GroupList().ToList();
+            }
             
-            mymodel.Group = objgroup.GroupList().ToList();
             mymodel.Format = objformat.FormatList().ToList();
             mymodel.LectureHall = objlecturehall.LectureHallList().ToList();
             mymodel.Note = objnote.NoteList().ToList();
@@ -37,6 +45,7 @@ namespace schedule.Controllers
         public IActionResult Update(int id, [Bind] Schedule model)
         {
             model.IdSchedule = id;
+           
             if (ModelState.IsValid)
             {
                 objschedule.UpdateSchedule(model);
